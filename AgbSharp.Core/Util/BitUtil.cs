8c8149d17d115f11ef91619512525dba79e9c447
OatmealDome <julian@oatmealdome.me>
2021-03-20 21:03:43 -0400

    Util: Add BitUtil with bit manipulation helpers

---
+++ AgbSharp.Core/Util/BitUtil.cs
@@ -0,0 +1,42 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AgbSharp.Core.Tests")]
+namespace AgbSharp.Core.Util
+{
+    internal class BitUtil
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetBit(uint b, int bit)
+        {
+            return (int)(b >> bit) & 0x1;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsBitSet(uint b, int bit)
+        {
+            return GetBit(b, bit) != 0;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetBit(ref uint b, int bit)
+        {
+            // for example, setting Carry on GbSharp CpuFlags:
+            // F    1000 0000
+            // mask 0001 0000
+            // XOR  1001 0000
+            b |= (uint)(1 << bit);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ClearBit(ref uint b, int bit)
+        {
+            // for example, clearing Carry on GbSharp CpuFlags:
+            // F      1001 0000
+            // mask   0001 0000
+            // invert 1110 0000
+            // AND    1000 0000
+            b &= (uint)~(1 << bit);
+        }
+
+    }
+}
