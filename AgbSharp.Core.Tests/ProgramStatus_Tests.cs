0a40f66eb4112f798f14c318825b96735fea58f3
OatmealDome <julian@oatmealdome.me>
2021-03-21 02:04:45 -0400

    Cpu: Add ProgramStatus to track CPSR/SPSR registers

---
+++ AgbSharp.Core.Tests/ProgramStatus_Tests.cs
@@ -0,0 +1,211 @@
+using AgbSharp.Core.Cpu.Status;
+using Xunit;
+
+namespace AgbSharp.Core.Tests
+{
+    public class ProgramStatus_Tests
+    {
+        [Fact]
+        public void Negative_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Negative = true;
+
+            Assert.True(status.Negative);
+            Assert.Equal((uint)0b10000000000000000000000000000000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void Zero_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Zero = true;
+
+            Assert.True(status.Zero);
+            Assert.Equal((uint)0b01000000000000000000000000000000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void Carry_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Carry = true;
+
+            Assert.True(status.Carry);
+            Assert.Equal((uint)0b00100000000000000000000000000000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void Overflow_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Overflow = true;
+
+            Assert.True(status.Overflow);
+            Assert.Equal((uint)0b00010000000000000000000000000000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void StickyOverflow_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.StickyOverflow = true;
+
+            Assert.True(status.StickyOverflow);
+            Assert.Equal((uint)0b00001000000000000000000000000000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void IrqDisable_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.IrqDisable = true;
+
+            Assert.True(status.IrqDisable);
+            Assert.Equal((uint)0b00000000000000000000000010000000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void FastIrqDisable_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.FastIrqDisable = true;
+
+            Assert.True(status.FastIrqDisable);
+            Assert.Equal((uint)0b00000000000000000000000001000000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void Thumb_SetTrue_BoolAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Thumb = true;
+
+            Assert.True(status.Thumb);
+            Assert.Equal((uint)0b00000000000000000000000000100000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void CpuMode_SetUser_PropertyAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Mode = CpuMode.User;
+
+            Assert.Equal(CpuMode.User, status.Mode);
+            Assert.Equal((uint)0b00000000000000000000000000010000, status.RegisterValue);
+        }
+
+        [Fact]
+        public void CpuMode_SetFastIrq_PropertyAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Mode = CpuMode.FastIrq;
+
+            Assert.Equal(CpuMode.FastIrq, status.Mode);
+            Assert.Equal((uint)0b00000000000000000000000000010001, status.RegisterValue);
+        }
+
+        [Fact]
+        public void CpuMode_SetIrq_PropertyAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Mode = CpuMode.Irq;
+
+            Assert.Equal(CpuMode.Irq, status.Mode);
+            Assert.Equal((uint)0b00000000000000000000000000010010, status.RegisterValue);
+        }
+
+        [Fact]
+        public void CpuMode_SetSupervisor_PropertyAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Mode = CpuMode.Supervisor;
+
+            Assert.Equal(CpuMode.Supervisor, status.Mode);
+            Assert.Equal((uint)0b00000000000000000000000000010011, status.RegisterValue);
+        }
+
+        [Fact]
+        public void CpuMode_SetAbort_PropertyAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Mode = CpuMode.Abort;
+
+            Assert.Equal(CpuMode.Abort, status.Mode);
+            Assert.Equal((uint)0b00000000000000000000000000010111, status.RegisterValue);
+        }
+
+        [Fact]
+        public void CpuMode_SetUndefined_PropertyAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Mode = CpuMode.Undefined;
+
+            Assert.Equal(CpuMode.Undefined, status.Mode);
+            Assert.Equal((uint)0b00000000000000000000000000011011, status.RegisterValue);
+        }
+
+        [Fact]
+        public void CpuMode_SetSystem_PropertyAndRegisterSetCorrectly()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Mode = CpuMode.System;
+
+            Assert.Equal(CpuMode.System, status.Mode);
+            Assert.Equal((uint)0b00000000000000000000000000011111, status.RegisterValue);
+        }
+        
+        [Fact]
+        public void RegisterValue_AllBoolsSetAndModeSystem_Correct()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.Negative = true;
+            status.Zero = true;
+            status.Carry = true;
+            status.Overflow = true;
+            status.StickyOverflow = true;
+            status.IrqDisable = true;
+            status.FastIrqDisable = true;
+            status.Thumb = true;
+            status.Mode = CpuMode.System;
+
+            Assert.Equal((uint)0b11111000000000000000000011111111, status.RegisterValue);
+        }
+
+        [Fact]
+        public void RegisterValue_SetDirectly_AllPropertiesCorrect()
+        {
+            ProgramStatus status = new ProgramStatus();
+
+            status.RegisterValue = (uint)0b11111000000000000000000011111111;
+
+            Assert.True(status.Negative);
+            Assert.True(status.Zero);
+            Assert.True(status.Carry);
+            Assert.True(status.Overflow);
+            Assert.True(status.StickyOverflow);
+            Assert.True(status.IrqDisable);
+            Assert.True(status.FastIrqDisable);
+            Assert.True(status.Thumb);
+            Assert.Equal(CpuMode.System, status.Mode);
+            Assert.Equal((uint)0b11111000000000000000000011111111, status.RegisterValue);
+        }
+
+    }
+}
---
+++ AgbSharp.Core/Cpu/Status/ProgramStatus.cs
@@ -0,0 +1,131 @@
+using AgbSharp.Core.Util;
+
+namespace AgbSharp.Core.Cpu.Status
+{
+    // CPSR and SPSR
+    class ProgramStatus
+    {
+        public bool Negative
+        {
+            get;
+            set;
+        }
+
+        public bool Zero
+        {
+            get;
+            set;
+        }
+
+        public bool Carry
+        {
+            get;
+            set;
+        }
+
+        public bool Overflow
+        {
+            get;
+            set;
+        }
+
+        // ARMv5 and up only
+        public bool StickyOverflow
+        {
+            get;
+            set;
+        }
+
+        public bool IrqDisable
+        {
+            get;
+            set;
+        }
+
+        public bool FastIrqDisable
+        {
+            get;
+            set;
+        }
+
+        public bool Thumb
+        {
+            get;
+            set;
+        }
+
+        public CpuMode Mode
+        {
+            get;
+            set;
+        }
+
+        public uint RegisterValue
+        {
+            get
+            {
+                uint i = 0;
+
+                if (Negative)
+                {
+                    BitUtil.SetBit(ref i, 31);
+                }
+
+                if (Zero)
+                {
+                    BitUtil.SetBit(ref i, 30);
+                }
+
+                if (Carry)
+                {
+                    BitUtil.SetBit(ref i, 29);
+                }
+
+                if (Overflow)
+                {
+                    BitUtil.SetBit(ref i, 28);
+                }
+
+                if (StickyOverflow)
+                {
+                    BitUtil.SetBit(ref i, 27);
+                }
+
+                // Bits 8 to 26 are reserved
+
+                if (IrqDisable)
+                {
+                    BitUtil.SetBit(ref i, 7);
+                }
+
+                if (FastIrqDisable)
+                {
+                    BitUtil.SetBit(ref i, 6);
+                }
+
+                if (Thumb)
+                {
+                    BitUtil.SetBit(ref i, 5);
+                }                
+
+                // CPU mode is bits 0 to 4, so just OR it
+                i |= (uint)Mode;
+
+                return i;
+            }
+            set
+            {
+                Negative = BitUtil.IsBitSet(value, 31);
+                Zero = BitUtil.IsBitSet(value, 30);
+                Carry = BitUtil.IsBitSet(value, 29);
+                Overflow = BitUtil.IsBitSet(value, 28);
+                StickyOverflow = BitUtil.IsBitSet(value, 27);
+                IrqDisable = BitUtil.IsBitSet(value, 7);
+                FastIrqDisable = BitUtil.IsBitSet(value, 6);
+                Thumb = BitUtil.IsBitSet(value, 5);
+                Mode = (CpuMode)(value & 0x1f);
+            }
+        }
+
+    }
+}
