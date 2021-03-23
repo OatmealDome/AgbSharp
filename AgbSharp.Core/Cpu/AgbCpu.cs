a0351c1164cec2712da94ae103f130d07b1d7c68
OatmealDome <julian@oatmealdome.me>
2021-03-23 18:21:17 -0400

    AgbCpu: Create ArmInterpreter instance and Step method

--- AgbSharp.Core/Cpu/AgbCpu.cs
+++ AgbSharp.Core/Cpu/AgbCpu.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using AgbSharp.Core.Cpu.Interpreter.Arm;
 using AgbSharp.Core.Cpu.Register;
 using AgbSharp.Core.Cpu.Status;
 using AgbSharp.Core.Memory;
@@ -25,6 +26,9 @@ namespace AgbSharp.Core.Cpu
         public IRegisterSet CurrentRegisterSet; // R0 to R15
         public ProgramStatus CurrentStatus; // CPSR
         public ProgramStatus CurrentSavedStatus; // SPSR
+
+        private ArmInterpreter ArmInterpreter;
+        // private ThumbInterpreter ThumbInterpreter;
 
         public AgbMemoryMap MemoryMap;
 
@@ -53,14 +57,33 @@ namespace AgbSharp.Core.Cpu
             CurrentRegisterSet = RegisterSets[CpuMode.User];
             CurrentStatus = new ProgramStatus();
             CurrentSavedStatus = null;
-            
+
+            ArmInterpreter = new ArmInterpreter(this);
+            // ThumbInterpreter = new ThumbInterpreter(this);
+
             MemoryMap = memoryMap;
         }
 
+        //
         // Helper function
+        //
         private ref uint Reg(int reg)
         {
             return ref CurrentRegisterSet.GetRegister(reg);
+        }
+
+        // Step the CPU by one instruction and return the cycles it took to execute
+        public int Step()
+        {
+            if (CurrentStatus.Thumb)
+            {
+                // return ThumbInterpreter.Step();
+                return 0;
+            }
+            else
+            {
+                return ArmInterpreter.Step();
+            }
         }
 
     }
