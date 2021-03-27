3c479d34b3e9df1fd17fea2054aa7ce25e742be1
OatmealDome <julian@oatmealdome.me>
2021-03-26 20:09:56 -0400

    ThumbInterpreter: Implement LSL, LSR, and ASR

---
+++ AgbSharp.Core.Tests/Cpu/Thumb/ThumbInterpreter_ShiftedRegister_Tests.cs
@@ -0,0 +1,63 @@
+using AgbSharp.Core.Cpu;
+using Xunit;
+
+namespace AgbSharp.Core.Tests.Cpu.Thumb
+{
+    public class ThumbInterpreter_ShiftedRegister_Tests
+    {
+        [Fact]
+        public void ShiftedRegister_LslRegOneWithResultInRegZero_RegZeroCorrect()
+        {
+            AgbCpu cpu = CpuUtil.CreateCpu();
+
+            cpu.CurrentRegisterSet.GetRegister(0) = 0xDEADBEEF;
+            cpu.CurrentRegisterSet.GetRegister(1) = 0x000001FF;
+
+            CpuUtil.RunCpu(cpu, new ushort[]
+            {
+                0x0806 // LSL r0, r1, #24
+            });
+
+            Assert.Equal((uint)0xFF000000, cpu.CurrentRegisterSet.GetRegister(0));
+            Assert.Equal((uint)0x000001FF, cpu.CurrentRegisterSet.GetRegister(1));
+            Assert.True(cpu.CurrentStatus.Carry);
+        }
+
+        [Fact]
+        public void ShiftedRegister_LsrRegOneWithResultInRegZero_RegZeroCorrect()
+        {
+            AgbCpu cpu = CpuUtil.CreateCpu();
+
+            cpu.CurrentRegisterSet.GetRegister(0) = 0xDEADBEEF;
+            cpu.CurrentRegisterSet.GetRegister(1) = 0xFF800000;
+
+            CpuUtil.RunCpu(cpu, new ushort[]
+            {
+                0x080e // LSR r0, r1, #24
+            });
+
+            Assert.Equal((uint)0x000000FF, cpu.CurrentRegisterSet.GetRegister(0));
+            Assert.Equal((uint)0xFF800000, cpu.CurrentRegisterSet.GetRegister(1));
+            Assert.True(cpu.CurrentStatus.Carry);
+        }
+
+        [Fact]
+        public void ShiftedRegister_AsrRegOneWithResultInRegZero_RegZeroCorrect()
+        {
+            AgbCpu cpu = CpuUtil.CreateCpu();
+
+            cpu.CurrentRegisterSet.GetRegister(0) = 0xDEADBEEF;
+            cpu.CurrentRegisterSet.GetRegister(1) = 0x801FF000;
+
+            CpuUtil.RunCpu(cpu, new ushort[]
+            {
+                0x4813 // ASR r0, r1, #8
+            });
+
+            Assert.Equal((uint)0xFFFC00FF, cpu.CurrentRegisterSet.GetRegister(0));
+            Assert.Equal((uint)0x801FF000, cpu.CurrentRegisterSet.GetRegister(1));
+            Assert.True(cpu.CurrentStatus.Carry);
+        }
+
+    }
+}
--- AgbSharp.Core/Cpu/Interpreter/Thumb/ThumbInterpreter.cs
+++ AgbSharp.Core/Cpu/Interpreter/Thumb/ThumbInterpreter.cs
@@ -1,3 +1,5 @@
+using AgbSharp.Core.Util;
+
 namespace AgbSharp.Core.Cpu.Interpreter.Thumb
 {
     class ThumbInterpreter : InstructionSetInterpreter
@@ -12,9 +14,53 @@ namespace AgbSharp.Core.Cpu.Interpreter.Thumb
             
             Reg(PC) += 2;
 
+            int type = BitUtil.GetBitRange(instruction, 13, 15);
+            switch (type)
+            {
+                case 0b000:
+                    return MoveShiftedRegister(instruction);
+            }
+
             InterpreterAssert($"Invalid instruction ({instruction:x4})");
 
             return 0;
+        }
+
+        private int MoveShiftedRegister(uint instruction)
+        {
+            ref uint sReg = ref Reg(BitUtil.GetBitRange(instruction, 3, 5));
+            ref uint dReg = ref Reg(BitUtil.GetBitRange(instruction, 0, 2));
+
+            int shift = BitUtil.GetBitRange(instruction, 6, 10);
+
+            int shiftType = BitUtil.GetBitRange(instruction, 11, 12);
+            ShiftType shiftTypeEnum;
+
+            switch (shiftType)
+            {
+                case 0b00:
+                    shiftTypeEnum = ShiftType.LogicalLeft;
+                    break;
+                case 0b01:
+                    shiftTypeEnum = ShiftType.LogicalRight;
+                    break;
+                case 0b10:
+                    shiftTypeEnum = ShiftType.ArithmaticRight;
+                    break;
+                default:
+                    InterpreterAssert("Invalid shift type");
+
+                    shiftTypeEnum = ShiftType.LogicalLeft;
+
+                    break;
+            }
+
+            dReg = PerformShift(shiftTypeEnum, sReg, shift, shift == 0, true);
+
+            CurrentStatus.Zero = dReg == 0;
+            CurrentStatus.Negative = BitUtil.IsBitSet(dReg, 31);
+
+            return 1; // 1S
         }
 
     }
