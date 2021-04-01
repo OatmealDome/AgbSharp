1d344c9e0779d0848b74d65b1612350ff514a734
OatmealDome <julian@oatmealdome.me>
2021-04-01 04:02:43 -0400

    MmioRegion: Move to Mmio namespace

--- AgbSharp.Core/Memory/AgbMemoryMap.cs
+++ AgbSharp.Core/Memory/AgbMemoryMap.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using AgbSharp.Core.Memory.Mmio;
 
 namespace AgbSharp.Core.Memory
 {
--- AgbSharp.Core/Memory/Mmio/MmioRegion.cs
+++ AgbSharp.Core/Memory/Mmio/MmioRegion.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-namespace AgbSharp.Core.Memory
+namespace AgbSharp.Core.Memory.Mmio
 {
     internal class MmioRegion : IMemoryRegion
     {
