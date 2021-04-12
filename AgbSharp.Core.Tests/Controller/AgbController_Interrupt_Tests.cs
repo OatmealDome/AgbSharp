9bff0790b0dda00cc496b3f068a42ba6b7874ba8
OatmealDome <julian@oatmealdome.me>
2021-04-12 19:00:44 -0400

    AgbController_Interrupt_Tests: Add additional tests for non-interrupting key states

--- AgbSharp.Core.Tests/Controller/AgbController_Interrupt_Tests.cs
+++ AgbSharp.Core.Tests/Controller/AgbController_Interrupt_Tests.cs
@@ -45,7 +45,25 @@ namespace AgbSharp.Core.Tests.Controller
         }
 
         [Fact]
-        public void UpdateKeyState_ChangeStateToPressedWithLogicalAndInterrupts_BitSet()
+        public void UpdateKeyState_ChangeUpStateToPressedWithLogicalOrInterrupts_CpuNotInterrupted()
+        {
+            AgbMemoryMap memoryMap = new AgbMemoryMap();
+            AgbCpu cpu = new AgbCpu(memoryMap);
+            AgbController controller = new AgbController(memoryMap, cpu);
+
+            memoryMap.WriteU32(0x4000208, 1); // IME = 1
+            memoryMap.WriteU16(0x4000200, 0x1000); // IE = Key
+            memoryMap.WriteU16(0x4000132, 0x4003); // Key interrupts enabled, logical OR, A or B
+
+            memoryMap.FlushMmio();
+
+            controller.UpdateKeyState(ControllerKey.Up, true);
+
+            Assert.Equal(CpuMode.User, cpu.CurrentStatus.Mode);
+        }
+
+        [Fact]
+        public void UpdateKeyState_ChangeStateToPressedWithLogicalAndInterrupts_CpuInterrupted()
         {
             AgbMemoryMap memoryMap = new AgbMemoryMap();
             AgbCpu cpu = new AgbCpu(memoryMap);
@@ -61,6 +79,24 @@ namespace AgbSharp.Core.Tests.Controller
             controller.UpdateKeyState(ControllerKey.B, true);
 
             Assert.Equal(CpuMode.Irq, cpu.CurrentStatus.Mode);
+        }
+
+        [Fact]
+        public void UpdateKeyState_OnlyAPressedWithLogicalAndInterrupts_CpuNotInterrupted()
+        {
+            AgbMemoryMap memoryMap = new AgbMemoryMap();
+            AgbCpu cpu = new AgbCpu(memoryMap);
+            AgbController controller = new AgbController(memoryMap, cpu);
+
+            memoryMap.WriteU32(0x4000208, 1); // IME = 1
+            memoryMap.WriteU16(0x4000200, 0x1000); // IE = Key
+            memoryMap.WriteU16(0x4000132, 0xC003); // Key interrupts enabled, logical AND, A and B
+
+            memoryMap.FlushMmio();
+
+            controller.UpdateKeyState(ControllerKey.A, true);
+
+            Assert.Equal(CpuMode.User, cpu.CurrentStatus.Mode);
         }
 
     }
